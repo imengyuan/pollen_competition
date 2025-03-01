8d1529cb498de10b5f5f0f644156d1dcf5153f5b
yuan <viciayuan@hotmail.com>
2025-02-28 22:02:53 -0500

    analyses in Jun

--- VCF_filtering/cross_TX_seed.sh
+++ VCF_filtering/cross_TX_seed.sh
@@ -15,5 +15,22 @@ tabix ${vcf_out_mom}
 bcftools merge ${vcf_out_mom} ${vcf_out_seed} --threads 20 \
 | bcftools filter -e 'FMT/DP="."' | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT\t%AD]\n' > ${vcf_out}
 
-
 sed -i 's/\,/\t/g' ${vcf_out}
+
+
+grep "A1" cross5_35h_02g_seed.vcf > cross5_35h_02g_seed_A1.vcf
+grep "A2" cross5_35h_02g_seed.vcf > cross5_35h_02g_seed_A2.vcf
+grep "A3" cross5_35h_02g_seed.vcf > cross5_35h_02g_seed_A3.vcf
+grep "A4" cross5_35h_02g_seed.vcf > cross5_35h_02g_seed_A4.vcf
+grep "X" cross5_35h_02g_seed.vcf > cross5_35h_02g_seed_X.vcf
+grep "Y" cross5_35h_02g_seed.vcf > cross5_35h_02g_seed_Y.vcf
+
+head -n 1 cross5_35h_02g_seed*.vcf
+
+python3  get_AF_pollen.py cross5_35h_02g_seed_A1.vcf A1 76150
+python3  get_AF_pollen.py cross5_35h_02g_seed_A2.vcf A2 13404
+python3  get_AF_pollen.py cross5_35h_02g_seed_A3.vcf A3 21757
+python3  get_AF_pollen.py cross5_35h_02g_seed_A4.vcf A4 7506
+python3  get_AF_pollen.py cross5_35h_02g_seed_X.vcf X 321
+python3  get_AF_pollen.py cross5_35h_02g_seed_Y.vcf Y 202888
+
