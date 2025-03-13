c12aa5bc5774afff63da072713c80ad9e189a6bd
yuan <viciayuan@hotmail.com>
2025-03-13 13:27:34 -0400

    update pollen AF and read count test

--- VCF_filtering/pollen_VCF_filt.sh
+++ VCF_filtering/pollen_VCF_filt.sh
@@ -1,53 +1,24 @@
 # extract ready-to-use VCF for each cross
-# cross2 02f	24a
-# maybe write a snakemake pipeline
-
-# testing pollen_vcf.sh on one sample
-id=2
-male=02f
-# DP threshold, 2*mean in the sample
-DP_leaf=38
-DP_pollen=160
-
-vcf_in=/ohta2/meng.yuan/rumex/pollen_competition/VCF_cross/TX_40samples_GT.vcf.gz
-vcf_leaf=/ohta2/meng.yuan/rumex/pollen_competition/cross_TX/${male}MLD.filt.vcf.gz
-vcf_pollen=/ohta2/meng.yuan/rumex/pollen_competition/cross_TX/${male}PD.filt.vcf.gz
-vcf_out=/ohta2/meng.yuan/rumex/pollen_competition/cross_TX/cross${id}_${male}_pollen.vcf
-
-# leaf
-filter=$(printf 'GQ>50 & FMT/DP<%d & GT="0/1" & FMT/DP>0 & ALT !="."' "$DP_leaf")
-bcftools view -m2 -M2 -s "${male}MLD" ${vcf_in} | bcftools filter -i "$filter" --threads 20 -O z -o ${vcf_leaf}
-
-# pollen
-filter=$(printf 'FMT/DP>50 & FMT/DP<%d' "$DP_pollen")
-bcftools view -s "${male}PD" ${vcf_in} | bcftools filter -i "$filter" --threads 20 -O z -o ${vcf_pollen}
-
-# index new VCFs
-tabix ${vcf_leaf}
-tabix ${vcf_pollen}
-
-# merge VCFs
-bcftools merge ${vcf_leaf} ${vcf_pollen}  --threads 20 \
-| bcftools filter -e 'FMT/DP="."' | bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT\t%AD]\n' > ${vcf_out}
-
-
 while read -r col1 col2 _ _ col5 col6 _; do
     sh pollen_vcf.sh "$col1" "$col2" "$col5" "$col6"
+    sed -i 's/\,/\t/g' cross${col1}_${col2}_pollen.vcf
 done < pollen_auto_cov.txt
 
-######################################################################
-# make separate VCF of each chrom for windowed analyses
-id=2
-dad=02f
-for i in "A1" "A2" "A3" "A4" "X" "Y"
-do
-grep ${i} cross${id}_${dad}_pollen.vcf > cross${id}_${dad}_pollen_${i}.vcf
-# get the sliding window start position for each chrom
-start_pos=$(head -n 1 cross${id}_${dad}_pollen_${i}.vcf | cut -f2)
+# make separate VCF of each chrom for windowed AF analyses
+while read -r col1 col2 _; do
+	for i in "A1" "A2" "A3" "A4"; do
+		grep ${i} cross${col1}_${col2}_pollen.vcf > cross${col1}_${col2}_pollen_${i}.vcf
+		start_pos=$(head -n 1 cross${col1}_${col2}_pollen_${i}.vcf | cut -f2)
+		python3 get_AF_pollen.py cross${col1}_${col2}_pollen_${i}.vcf ${i} ${start_pos}
+	done
+	cat cross${col1}_${col2}_pollen_*.vcf.freq > cross${col1}_${col2}_pollen.vcf.freq
+	rm cross${col1}_${col2}_pollen_A*.vcf
+	rm cross${col1}_${col2}_pollen_A*.vcf.freq
+done < pollen_auto_cov.txt
 
-python3  get_AF_pollen.py cross${id}_${dad}_pollen_${i}.vcf ${i} ${start_pos} 
-done
 
-cat cross${id}_${dad}_pollen_*.vcf.freq > cross${id}_${dad}_pollen.vcf.freq
-
+# perform fisher's exact test on AD of leaf and pollen
+while read -r col1 col2 _; do
+	/usr/local/bin/Rscript --vanilla pollen_AF_test.R "$col1" "$col2"
+done < pollen_auto_cov.txt
 
---
+++ get_AF/pollen_AF_test.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(data.table)
+library(stringr)
+setwd("/ohta2/meng.yuan/rumex/pollen_competition/cross_TX")
+args = commandArgs(trailingOnly=TRUE)
+id <- paste0("cross", args[1], "_", args[2])
+
+fisher_test <- function(dad0, dad1, pollen0, pollen1) {
+    values <- c(dad0, dad1, pollen0, pollen1)
+    contingency_table <- matrix(values, nrow = 2, byrow = TRUE)
+    test_result <- fisher.test(contingency_table)
+    odds_ratio <- test_result$estimate
+    p_value <- test_result$p.value
+    return(list(odds_ratio = odds_ratio, p_value = p_value))
+}
+
+cross <- read.table(paste0(id,"_pollen.vcf"))
+colnames(cross) <- c("chrom", "pos", "ref", "alt", "dad", "dad0", "dad1", "pollen", "pollen0", "pollen1")
+cross <- cross %>% filter(str_starts(chrom, "A"))
+cross <- as.data.table(cross)
+cross[, c("odds_ratio", "p_value") := fisher_test(dad0, dad1, pollen0, pollen1), by = 1:nrow(cross)]
+write.table(cross, file=paste0(id,"_test.txt"), quote = F, row.names = F,sep = "\t")
