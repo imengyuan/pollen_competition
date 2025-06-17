3d45914760e4908f36c5d574301dbc955911094a
yuan <viciayuan@hotmail.com>
2025-06-17 15:27:13 -0400

    add pollen AF test scripts

--- get_AF/pollen_AF_test.sh
+++ get_AF/pollen_AF_test.sh
@@ -6,7 +6,7 @@ done < pollen_auto_cov.txt
 
 # only need chrom, pos, p_value for plotting
 while read -r col1 col2 _; do
-	#cut -f 1,2,12 cross${col1}_${col2}_test.txt > cross${col1}_${col2}_test.tmp
+	cut -f 1,2,12 cross${col1}_${col2}_test.txt > cross${col1}_${col2}_test.tmp
 	/usr/local/bin/Rscript --vanilla pollen_AF_plot.R "$col1" "$col2"
 done < pollen_auto_cov.txt
 
