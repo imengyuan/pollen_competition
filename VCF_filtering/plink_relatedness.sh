9cad008f534d7d0cb8b8da71407b9c1d378d4aaa
yuan <viciayuan@hotmail.com>
2025-07-28 23:19:25 -0400

    PCA and genetic relatedness matrix

---
+++ VCF_filtering/plink_relatedness.sh
@@ -0,0 +1,59 @@
+
+
+# filter for SNPs
+vcf_in=/ohta2/meng.yuan/rumex/pollen_competition/VCF_cross/TX_40samples_GT.vcf.gz
+vcf_out=/ohta2/meng.yuan/rumex/pollen_competition/VCF_cross/TX_40samples_GT.filt.vcf.gz
+
+
+gunzip ${vcf_in} 
+bgzip TX_40samples_GT.vcf 
+tabix ${vcf_in}
+
+vcftools --gzvcf ${vcf_in} \
+--mac 1 \
+--remove-indels \
+--max-missing 1 \
+--min-alleles 2 --max-alleles 2 \
+--minQ 30 \
+--min-meanDP 10 \
+--max-meanDP 200 \
+--recode --stdout | bgzip -c > ${vcf_out}
+
+tabix ${vcf_out}
+
+
+# plink
+VCF=/ohta2/meng.yuan/rumex/pollen_competition/VCF_cross/TX_40samples_GT.filt.vcf.gz
+# 
+
+
+# make bed file
+i=TX_40samples
+plink --vcf $VCF --double-id --allow-extra-chr  \
+--keep-allele-order --set-missing-var-ids @:# \
+--maf 0.05 --geno 0.2 --hwe 1e-6 \
+--biallelic-only strict \
+--make-bed --out ${i}
+
+
+# LD pruning
+plink --bfile ${i} --indep-pairwise 200 50 0.1 \
+--allow-extra-chr --keep-allele-order --out ${i}_ld
+
+
+# pca using plink
+plink --bfile ${i} --double-id --allow-extra-chr --set-missing-var-ids @:# \
+--extract ${i}_ld.prune.in --pca --out ${i}_ld
+
+
+# get LD pruned files
+plink --bfile ${i} --extract ${i}_ld.prune.in \
+--allow-extra-chr --keep-allele-order --make-bed --out ${i}_ld
+
+
+# related matrix 
+plink --bfile ${i}_ld --make-rel triangle --out ${i}_grm 
+
+plink --bfile ${i}_ld -make-grm-gz no-gz --out ${i}_grm
+
+
